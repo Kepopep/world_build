5eafafee1ad21e13d44a9a84d10fc875cea3b63c
HiSder <hisder311@gmail.com>
2026-09-10 11:06:06 +0300

    feat!: delete all

--- src/main/java/com/hisder/worldBuilding/world/contract/WorldCreateRequest.java
+++
@@ -1,8 +0,0 @@
-package com.hisder.worldBuilding.world.contract;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record WorldCreateRequest(
-        @NotBlank(message = "name cannot be blank")
-        String name
-) {}
